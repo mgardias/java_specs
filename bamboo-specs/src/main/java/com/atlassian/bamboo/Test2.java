38f2b39f47a32e7763311dbf848cce24aa98d658
Marcin Gardias <mgardias@atlassian.com>
2024-11-12 11:41:58 +0100

    new test file

---
+++ bamboo-specs/src/main/java/com/atlassian/bamboo/Test2.java
@@ -0,0 +1,8 @@
+package com.atlassian.bamboo;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class Test2 {
+    private static final org.apache.logging.log4j.Logger log = org.apache.logging.log4j.LogManager.getLogger(Test2.class);
+}
